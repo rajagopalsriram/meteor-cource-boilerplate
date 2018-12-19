63c0225014fff2468bfa63e63b35001f84a569b4
raju <rajagopalsriram13693@gmail.com>
2018-12-19 10:49:58 +0530

    first commit

---
+++ imports/ui/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import PrivateHeader from './PrivateHeader';
+
+
+export default () => {
+  return (
+    <div>
+      <PrivateHeader title="Dashboard"/>
+      <div className="page-content"> 
+      	Dashboard page content.
+      </div>
+      
+     
+    </div>
+  );
+};
---
+++ readme.md
@@ -0,0 +1,8 @@
+This is shortener url build for meteor and react;
+# Short url
+
+## Author
+
+### Raju new app
+
+# This is new
